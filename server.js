1cc27216779c07f7e319de80f8d8e873a8780ad4
Diane Galiano <dianegaliano09@gmail.com>
2021-12-21 12:21:46 -0600

    deploy

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
 
-const PORT = process.env.port || 3000;
+const PORT = process.env.port || 3001;
 
 const app = express();
 
